e3ee67083ce0285e9063cf585f683b89f74da93b
alon <alon.glater@protonmail.com>
2019-04-22 08:02:58 +0300

    Add typescript to project.

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "./dist/bundle.js",
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ]
+    }
+};
